84ac0d79db240820ba6ba2eb0a817edbe847e9ff
Jakub Kirker <jakubkir@wp.pl>
2021-10-27 21:35:23 +0200

    6.2 Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,16 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(7, 5);
+        int sub = calculator.substract(8, 6);
+        if (sum == 12 && sub == 2) {
+            System.out.println("Calculator is ok");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,41 @@
+package com.kodilla.testing.collection;
+
+import org.junit.*;
+import java.util.*;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+
+public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Test case: begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test case: end");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        //Given
+        ArrayList<Integer> emptyList=new ArrayList<Integer>();
+        //When
+        ArrayList<Integer> listNumbers=OddNumbersExterminator.exterminate(emptyList);
+        System.out.println("Testing"+listNumbers);
+        //Then
+        Assert.assertEquals(emptyList,listNumbers);
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+        //Given
+        ArrayList<Integer> list=new ArrayList<Integer>(Arrays.asList(34,777,22,199,18,15,3,12,4));
+        ArrayList<Integer> oddList=new ArrayList<Integer>(Arrays.asList(34,22,12,18));
+
+        //When
+        ArrayList<Integer> listNumbers=OddNumbersExterminator.exterminate(list);
+        System.out.println("Testing"+listNumbers);
+        //Then
+        Assert.assertEquals(oddList,listNumbers);
+    }
+}
